dc89d0ebc274acc410792932f52a01448a8367d7
Guitarwang01 <11912420@mail.sustech.edu.cn>
2024-06-19 22:48:11 +0900

    task08 finished! Very cool code and I enjoyed this task! Thank you professor!

--- task08/main.py
+++ task08/main.py
@@ -11,7 +11,6 @@ import parse_gltf
 import util_for_task08
 
 
-
 class HelloWorld(mglw.WindowConfig):
     '''
     Window to show the gltf animation
@@ -94,7 +93,7 @@ class HelloWorld(mglw.WindowConfig):
                 continue
             # below, write one or two lines of code to compute `bone2globalTransformation[i_bone]`
             # hint: use numpy.matmul for multiplying nd-array
-            # bone2globalTransformation[i_bone] = ???
+            bone2globalTransformation[i_bone] = np.matmul(bone2globalTransformation[i_bone_parent], bone2relativeTransformation[i_bone])
 
         for i_vtx in range(self.vtx2xyz_ini.shape[0]):  # for each point in mesh
             p0 = self.vtx2xyz_ini[i_vtx]
@@ -108,8 +107,9 @@ class HelloWorld(mglw.WindowConfig):
                 # write a few lines of codes to compute p1 using the linear blend skinning
                 # hint: use np.matmul for matrix multiplication
                 # hint: assume that rig weights w add up to one
-
-                # p1 += ???
+                Ai = np.matmul(globalTransformation, inverseBindingMatrix)
+                p1 += w*np.matmul(Ai, p0)
+                
 
             self.vtx2xyz_def[i_vtx] = p1[:3]  # from homogeneous coordinates to the Cartesian coordinates
 
